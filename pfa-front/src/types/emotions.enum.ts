5afb20867e1a80b545d30135c9b20a17a41e2b42
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2025-05-31 22:05:27 +0100

    add emotions enum

---
+++ pfa-front/src/types/emotions.enum.ts
@@ -0,0 +1,9 @@
+enum Emotion {
+  Joy = "Joie",
+  Sad = "Tristesse",
+  Angry = "Colère",
+  Surprised = "Surprise",
+  Neutral = "Neutre",
+  Uncertain = "Incertitude",
+}
+export default Emotion;
